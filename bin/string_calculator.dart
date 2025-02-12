e9126b350b4d1d50b568f7890da39739b2fef49b
atishayjain740 <atishayjain740@gmail.com>
2025-02-11 23:00:31 -0800

    Automatically run tests when executing the project

--- bin/string_calculator.dart
+++ bin/string_calculator.dart
@@ -1,1 +1,20 @@
+import 'dart:io';
 
+Future<void> runTests() async {
+  print("Running all test cases...");
+
+  final result = await Process.run('dart', ['test']);
+  print(result.stdout); // test output
+  print(result.stderr); // errors if any
+
+  if (result.exitCode != 0) {
+    print("Tests failed! Stopping execution.");
+    exit(1); // Stop execution if tests fail
+  }
+
+  print("All tests passed!");
+}
+
+void main() async {
+  await runTests(); // Run tests
+}
