e18561da5c4100a889f24bc90a7adb10200d3844
atishayjain740 <atishayjain740@gmail.com>
2025-02-11 22:16:53 -0800

    Handling custom single character delimiter

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -3,8 +3,26 @@ class StringCalculator {
     // For empty strings return 0
     if (numbers.isEmpty) return 0;
 
-    // Split the string into a list of strings based on comma and new line
-    List<String> numberStrings = numbers.split(RegExp(r",|\n"));
+    // For comma and new line
+    RegExp delimiterRegex = RegExp(r",|\n");
+
+    // Check if the input starts with "//" (indicating a custom delimiter)
+    if (numbers.startsWith("//")) {
+      // Split the input into two parts: the delimiter definition and the numbers
+      final parts = numbers.split('\n');
+
+      // removing the "//" prefix
+      String customDelimiter = parts[0].substring(2);
+
+      // Update the delimiter regex to use the custom delimiter
+      delimiterRegex = RegExp(RegExp.escape(customDelimiter));
+
+      // Update the numbers string to exclude the delimiter definition line
+      numbers = parts.sublist(1).join('\n');
+    }
+
+    // Split the string into a list of strings based on the delimiter regex.
+    List<String> numberStrings = numbers.split(delimiterRegex);
 
     // Convert it into a list of integers
     List<int> numberList = numberStrings.map((str) => int.parse(str)).toList();
--- test/string_calculator_test.dart
+++ test/string_calculator_test.dart
@@ -27,4 +27,8 @@ void main() {
   test('Handles new lines as delimiters', () {
     expect(calculator.add("1\n2,3"), equals(6));
   });
+
+  test('Supports custom single-character delimiters', () {
+    expect(calculator.add("//;\n1;2"), equals(3));
+  });
 }
